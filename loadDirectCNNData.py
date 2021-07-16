d77034aff4f5f745791ad69d1b63528595144048
STephen Schmidt <schmidt.st@husky.neu.edu>
2021-07-15 20:38:13 -0400

    set up flat training data for new CNN

--- loadDirectCNNData.py
+++ loadDirectCNNData.py
@@ -24,8 +24,9 @@ def single_to_one_hot(labels, num_classes):
 
 class DirectCNNDataset():
     
-    def __init__(self, num_classes=4):
+    def __init__(self, num_classes=4, spect_shape = 593):
         
+        self.spec_path = os.path.join(os.path.dirname(os.getcwd()), 'Data')
         self.train_data_path = os.path.join(os.path.dirname(os.getcwd()), '89AccuracyOut')
         #self.train_data_path = os.path.join(os.path.dirname(os.getcwd()), 'MICNN_Out')
         
@@ -35,12 +36,6 @@ class DirectCNNDataset():
         
         self.num_classes=num_classes
         
-        self.train_core_spec = {}
-        self.train_core_true_sub_labels = {}
-        self.train_core_true_label = {}
-        self.train_core_pred_sub_labels = {}
-        self.train_core_probability_labels = {}
-        
         self.test_core_spec = {}
         self.test_core_true_sub_labels = {}
         self.test_core_true_label = {}
@@ -48,23 +43,69 @@ class DirectCNNDataset():
         self.test_core_probability_labels = {}
         self.test_positions = {}
         
+        class_label_count = {}
+        for i in range(num_classes):
+            class_label_count[i] = 0
+        
         for core in self.train_data.cores_list:
-   
-            if 'Label' not in core:
-                
+            if 'Label' not in core: 
                 label_filename = core + "_multiclass.hdf5"
                 label_file_path = os.path.join(self.train_data_path, label_filename)
+                
                 with h5py.File(label_file_path, "r") as hf:
                     dname = list(hf.keys())[0]
                     n1 = hf.get(dname)    
                     n1_array = np.copy(n1)
                     
-                    one_hot_labels_array = single_to_one_hot(n1_array, 4)
-                
-                self.train_core_spec[core] = self.train_data.train_data[core]
-                self.train_core_true_label[core] = int(self.train_data.train_data[core +'_Labels'][0])
-                self.train_core_pred_sub_labels[core] = one_hot_labels_array.astype(int)
-                self.train_core_probability_labels[core] = np.zeros((self.train_data.train_data[core].shape[0], self.num_classes))
+                    label_locations = np.where(n1_array != 0)[0]
+                    count = label_locations.shape[0]
+                    if count == 0:
+                        label = 0
+                        count = n1_array.shape[0]
+                    else:
+                    
+                        label = int(n1_array[label_locations][0])
+                    
+                    class_label_count[label] += count
+        
+        self.spec_dict = {}
+        index_dict = {}
+        for i in range(num_classes):
+            self.spec_dict[i] = np.zeros((class_label_count[i], spect_shape))
+            index_dict[i] = 0  
+        
+        
+        # put each labeled location into its own array so it can be flat
+        
+        for core in self.train_data.cores_list:
+            if 'Label' not in core: 
+                label_filename = core + "_multiclass.hdf5"
+                label_file_path = os.path.join(self.train_data_path, label_filename)
+                spec_path = os.path.join(self.spec_path, core + '.hdf5')
+                with h5py.File(label_file_path, "r") as hf:
+                    with h5py.File(spec_path, "r") as hf_spec:
+                        dname = list(hf.keys())[0]
+                        n1 = hf.get(dname)    
+                        label_array = np.copy(n1)
+                        
+                        dname = list(hf_spec.keys())[0]
+                        n1 = hf_spec.get(dname)    
+                        spec_array = np.copy(n1)
+                                
+                        label_locations = np.where(label_array != 0)
+                        count = label_locations[0].shape[0]
+                        if count == 0:
+                            label = 0
+                            count = label_array.shape[0]
+                        else:
+                            label = int(label_array[label_locations][0])
+                   
+                        if label == 0:
+                            self.spec_dict[label][index_dict[label]:index_dict[label]+count, 0:591] = spec_array
+                        else:
+                            spec_wanted = np.take(spec_array, label_locations, 0)
+                            self.spec_dict[label][index_dict[label]:index_dict[label]+count, 0:591] = spec_wanted
+                        index_dict[label] += count     
                 
         
         for core in self.test_data.cores_list:
@@ -76,12 +117,6 @@ class DirectCNNDataset():
             self.test_core_probability_labels[core] = np.zeros((self.test_data.tissue_labels[core_positions].shape[0], self.num_classes))
             self.test_positions[core] = self.test_data.position[core_positions].astype(int)
             
-            
-
-        
-        
-        
-        
 
 
 dset = DirectCNNDataset(num_classes=4)
